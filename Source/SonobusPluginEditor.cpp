efb9990e4d763567043127eabefc75bcd99b769c
Steven Wilson <2165238+mendeni@users.noreply.github.com>
2025-08-24 00:16:47 -0400

    Session output of recordRoundtripLatencyForAll

--- Source/SonobusPluginEditor.cpp
+++ Source/SonobusPluginEditor.cpp
@@ -2432,7 +2432,8 @@ void SonobusAudioProcessorEditor::recordRoundtripLatencyForAll()
 
     std::cout << std::endl;
     std::cout << "*** Begin Capturing Latency ***" << std::endl;
-    std::cout << Time::getCurrentTime().formatted("%Y-%m-%d_%H.%M.%S") << std::endl;
+    std::cout << "Session:" << processor.getCurrentJoinedGroup() << std::endl;
+    std::cout << "Date:" << Time::getCurrentTime().formatted("%Y-%m-%d_%H.%M.%S") << std::endl;
 
     // do it for everyone
     for (int j=0; j < processor.getNumberRemotePeers(); ++j)
