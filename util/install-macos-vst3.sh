0ec97728f85b1828e047a92fa2be4052285450e8
Steven Wilson <2165238+mendeni@users.noreply.github.com>
2026-01-20 06:37:09 -0500

    Harden install-macos-vst3.sh

--- util/install-macos-vst3.sh
+++ util/install-macos-vst3.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+if [ $USER != 'root' ]; then
+  echo "/************************************/"
+  echo "/*                                  */"
+  echo "/* This script must be run as root: */"
+  echo "/*                                  */"
+  echo "/*   sudo ./install-macos-vst3.sh   */"
+  echo "/*                                  */"
+  echo "/************************************/"
+  exit 1
+fi
+
 TARGET='/Library/Audio/Plug-Ins/VST3/SonoBusMendeni'
 
 if [ ! -d $TARGET ]; then
@@ -8,15 +19,20 @@ fi
 
 if [ ! -d 'VST3' ]; then
   echo "VST3 folder not found, exiting"
-  exit 1
+  exit 2
 fi
 
 cd VST3 && cp -a * $TARGET
 
-codesign --force --deep --sign - $TARGET/SonoBusMendeni.vst3/Contents/MacOS/SonoBusMendeni
+codesign --force --deep --sign - $TARGET/SonoBusMendeni.vst3/Contents/MacOS/SonoBusMendeni || exit 3
 
 spctl --master-disable
 spctl --status
 
-echo "Don't forget to re-enable afer install"
-echo "spctl --master-enable"
+echo "/****************************************/"
+echo "/*                                      */"
+echo "/* Don't forget to re-enable Gatekeeper */"
+echo "/*                                      */"
+echo "/*      sudo spctl --master-enable      */"
+echo "/*                                      */"
+echo "/****************************************/"
