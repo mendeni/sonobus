89aeae1372fcdf072cdedf3cbdb2104692ae9fb4
Steven Wilson <2165238+mendeni@users.noreply.github.com>
2026-01-23 12:48:20 -0500

    OSC use UDP 6000, 6001 to resolve port conflicts

--- Source/SonobusPluginProcessor.cpp
+++ Source/SonobusPluginProcessor.cpp
@@ -843,22 +843,22 @@ mState (*this, &mUndoManager, "SonoBusAoO",
     initializeAoo();
 
     // Initialize the OSC receiver
-    const int receivePort = 9000;
+    const int receivePort = 6000;
     if (!oscManager.initializeReceiver(receivePort))
     {
         juce::Logger::writeToLog("Failed to initialize OSC Receiver.");
     }
 
     // Initialize the OSC sender
-    const juce::String targetIPAddress = "10.10.10.10"; // Replace with actual destination
-    const int targetPort = 8000;
+    const juce::String targetIPAddress = "127.0.0.1"; // Replace with actual destination
+    const int targetPort = 6001;
     if (!oscManager.initializeSender(targetIPAddress, targetPort))
     {
         juce::Logger::writeToLog("Failed to initialize OSC Sender.");
     }
 
     // Example: Send a test message
-    // oscManager.sendMessage("/testMessage", 123);
+    oscManager.sendMessage("/testMessage", 123);
 
     mFreshInit = false; // need to ensure this before loaddefaultpluginstate
 
