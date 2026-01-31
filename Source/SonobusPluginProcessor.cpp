3e3001057485fb05c37e698b733dfc4117e21a27
Steven Wilson <2165238+mendeni@users.noreply.github.com>
2026-01-31 15:23:36 -0500

    Allow input reverb for up to 4 channels

--- Source/SonobusPluginProcessor.cpp
+++ Source/SonobusPluginProcessor.cpp
@@ -7806,11 +7806,9 @@ void SonobusAudioProcessor::processBlock (AudioBuffer<float>& buffer, MidiBuffer
             inputRevBuffer.applyGainRamp(0, numSamples, sgain, egain);
         }
 
-        if (sendCh == 1 || sendCh == 2) {
-            // mix it into send workbuffer
-            for (int channel = 0; channel < sendPanChannels && channel < 2; ++channel) {
-                sendWorkBuffer.addFrom(channel, 0, inputRevBuffer, channel, 0, numSamples);
-            }
+        // mix it into send workbuffer for each channel up to 4
+        for (int channel = 0; channel < sendPanChannels && channel < 4; ++channel) {
+            sendWorkBuffer.addFrom(channel, 0, inputRevBuffer, channel, 0, numSamples);
         }
     }
 
